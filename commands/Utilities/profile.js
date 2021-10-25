e943b1299ce2fe71755ed224de9151ee0b75bf35
HansAnonymous <hansolo.games@gmail.com>
2021-10-25 02:44:54 -0700

    update 9! adapted to new premium update

--- commands/Utilities/profile.js
+++ commands/Utilities/profile.js
@@ -17,6 +17,9 @@ const cookies = [{
 }]
 
 var profile = {
+    premium: false,
+    moderator: false,
+    verified: false,
     thumbnail: "",
     created: "",
     displayName: "",
@@ -24,6 +27,7 @@ var profile = {
     url: "",
     tagline: "",
     description: "",
+    soundtrack: "",
     grade: "",
     networth: "",
     career: "",
@@ -72,17 +76,29 @@ module.exports = {
                     } else {
                         const html = await page.content();
                         const $ = cheerio.load(html);
-                        profile.thumbnail = 'https:' + $('body > div.pt-16 > div > main > div.relative > div > div.bg-white.shadow > div.pt-2.p-5 > div.flex.justify-center > div > img').attr('src').replace("https:", "");
-                        profile.created = $('main > div.relative > div > div.bg-white > div.px-5.ml-3.flex.items-start.text-xs.pb-6.mt-3 > span.text-gray-700.ml-2').text().replace('\n', ' ').replace(/\s\s+/g, ' ');
+                        profile.premium = $('main > div.relative > div > div > div > div.flex.justify-center > div > div.font-bold > div > div > span.tooltip').length ? true : false;
+                        profile.moderator = $('main > div.relative > div > div.bg-white > div.pt-2.p-5 > div.flex.justify-center > div > div > div > div.inline-flex.text-gray-400 > svg').length ? true : false;
+                        profile.verified = $('main > div.relative > div > div.bg-white > div.pt-2.p-5 > div.flex.justify-center > div > div.font-bold > div > div:nth-child(2) > svg').length ? true : false;
+                        if(profile.premium) {
+                            profile.career = $('main > div.relative > div > div:nth-child(3) > h5 > span').text().trim();
+                            profile.networth = $('main > div.relative > div > div:nth-child(5) > dl > div > div > dd > a:nth-child(1)').text()
+                            profile.shlinks =  $('main > div.relative > div > div:nth-child(5) > dl > a:nth-child(2) > div > div > dd').text().trim();
+                            profile.views = $('main > div.relative > div > div:nth-child(5) > dl > a:nth-child(3) > div > div > dd').text().trim();
+                            profile.soundtrack = ('' + $('#soundtrack').attr('src')).replace('embed/', '').replace('?theme=0', '');
+                        } else {
+                            profile.career = $('main > div.relative > div > div:nth-child(2) > h5 > span').text().trim();
+                            profile.networth = $('main > div.relative > div > div:nth-child(4) > dl > div > div > dd > a:nth-child(1)').text()
+                            profile.shlinks = $('main > div.relative > div > div:nth-child(4) > dl > a:nth-child(2) > div > div > dd').text().trim();
+                            profile.views = $('main > div.relative > div > div:nth-child(4) > dl > a:nth-child(3) > div > div > dd').text().trim();
+                        }
+                        profile.thumbnail = 'https:' + ('' + $('main > div.relative > div > div.bg-white.shadow > div.pt-2.p-5 > div.flex.justify-center.relative > div > img').prop('src')).replace("https:", "");
+                        //profile.created = $('main > div.relative > div > div.bg-white > div.px-5.ml-3.flex.items-start.text-xs.pb-6.mt-3 > span.text-gray-700.ml-2').text().replace('\n', ' ').replace(/\s\s+/g, ' ');
+                        profile.created = $('main > div.relative > div > div.bg-white > div.flex.justify-between > div > span.text-gray-700.ml-2').text().replace('\n', ' ').replace(/\s\s+/g, ' ');
                         profile.displayName = $('body > div.pt-16 > div > main > div.relative > div > div.bg-white.shadow > div.pt-2.p-5 > div.flex.justify-center > div.ml-3.mt-2 > div.font-bold').text().split('\n')[2];
                         profile.tagline = $('body > div.pt-16 > div > main > div.relative > div > div.bg-white.shadow > div.pt-2.p-5 > div.flex.justify-center > div.ml-3.mt-2 > div > span.text-gray-700').text()
                         let description = $('body > div.pt-16 > div > main > div.relative > div > div.bg-white.shadow > div.px-5.mt-1 > div > span.text-gray-700.italic').text()
                         profile.description = (description.length < 51) ? description : description.substring(0, 50) + '...';
                         profile.grade = $('body > div.pt-16 > div > main > div.relative > div > div.bg-white.shadow > div.pt-2.p-5 > div.flex.justify-center > div.ml-3.mt-2 > p.text-lg').text().trim();
-                        profile.networth = $('main > div.relative > div > div:nth-child(4) > dl > div > div > dd > a:nth-child(1)').text()
-                        profile.career = $('main > div.relative > div > div:nth-child(2) > h5 > span').text().trim();
-                        profile.shlinks = $('main > div.relative > div > div:nth-child(4) > dl > a:nth-child(2) > div > div > dd').text().trim();
-                        profile.views = $('main > div.relative > div > div:nth-child(4) > dl > a:nth-child(3) > div > div > dd').text().trim();
                     }
 
                     profile.lastUpdate = Date.now();
@@ -108,29 +124,38 @@ module.exports = {
                 //const timesince = Date.now() - profile.lastUpdate;
                 //const timetext = (timesince < 60 * 1000) ? "\nLast updated: Now" : "\nLast updated: " + Math.floor(timesince/(60 * 60 * 1000)) + " hours, " + Math.floor((timesince/(60 * 1000))%60) + " minutes ago";
                 //const timetext = ` <t:${profile.lastUpdate}:R>`
+
+                let embedDisplayName = profile.moderator ? profile.displayName + ' :shield:' : profile.displayName;
+                embedDisplayName = profile.premium ? embedDisplayName + ' <:platinum:902113910365249576>' : embedDisplayName;
+                embedDisplayName = profile.verified ? embedDisplayName + ' <:verified:902127233144078356>' : embedDisplayName;
                 
                 return message.reply(randomQuip(), new MessageEmbed()
                     .setColor(ee.color)
                     .setFooter(ee.footertext, ee.footericon)
                     .setTitle(`Retrieving Shlinkfiles...`)
                 ).then( msg => {
-                    msg.edit(new MessageEmbed()
-                    .setColor(ee.color)
-                    .setThumbnail(profile.thumbnail)
-                    .setFooter(profile.created, ee.footericon)
-                    .setTitle(profile.displayName)
-                    .setURL(profile.url)
-                    .setAuthor(profile.tagline)
-                    .setDescription(profile.description)
-                    .setTimestamp(profile.lastUpdate)
-                    .addField('Username', profile.username, true)
-                    .addField('Life Grade', profile.grade, true)
-                    .addField('Net Worth', profile.networth, true)
-                    .addField('Career Progress', profile.career, true)
-                    .addField('Shlinks', profile.shlinks, true)
-                    .addField('Profile Views', profile.views, true),
-                    { buttons: [ button1, button2, button3 ]}
-                    );
+                    let profileEmbed = new MessageEmbed()
+                        .setColor(ee.color)
+                        .setThumbnail(profile.thumbnail)
+                        .setFooter(profile.created, ee.footericon)
+                        .setTitle(embedDisplayName)
+                        .setURL(profile.url)
+                        .setAuthor(profile.tagline)
+                        .setDescription(profile.description)
+                        .setTimestamp(profile.lastUpdate);
+                    if(profile.premium) {
+                        profileEmbed.addField('Soundtrack', `[Spotify](${profile.soundtrack})`, false)
+                    }
+                    profileEmbed
+                        .addField('Username', profile.username, true)
+                        .addField('Life Grade', profile.grade, true)
+                        .addField('Net Worth', profile.networth, true)
+                        .addField('Career Progress', profile.career, true)
+                        .addField('Shlinks', profile.shlinks, true)
+                        .addField('Profile Views', profile.views, true),
+                        { buttons: [ button1, button2, button3 ]}
+
+                    msg.edit(profileEmbed);
                 });
             } else {
                 return message.channel.send(new MessageEmbed()
